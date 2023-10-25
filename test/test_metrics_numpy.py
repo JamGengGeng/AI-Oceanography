704885db289e7dcc2b7e299500eb22708cc44d57
Isotr0py <2037008807@qq.com>
2023-10-25 09:35:23 +0800

    simplify test

--- test/test_metrics_numpy.py
+++ test/test_metrics_numpy.py
@@ -1,16 +1,17 @@
+from functools import partial
+
 import numpy as np
-from ml_metrics.metrics_numpy import (F1, MAE, MSE, R2, RMSE, Accuracy,
-                                      Precision, Recall)
-from sklearn.metrics import (accuracy_score, f1_score, mean_absolute_error,
-                             mean_squared_error, precision_score, r2_score,
-                             recall_score)
-
-
-def test_MSE():
-    y = np.array([1, 2, 3, 4, 5, 6, 7])
-    y_hat = np.array([6, 5, 4, 2, 7, 8, 9])
-    mse = MSE()
-    assert mse(y, y_hat) == mean_squared_error(y, y_hat)
+import pytest
+from ml_metrics.metrics_numpy import F1, MAE, MSE, R2, RMSE, Accuracy, Precision, Recall
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    mean_absolute_error,
+    mean_squared_error,
+    precision_score,
+    r2_score,
+    recall_score,
+)
 
 
 def test_RMSE():
@@ -20,44 +21,32 @@ def test_RMSE():
     assert rmse(y, y_hat) == mean_squared_error(y, y_hat) ** 0.5
 
 
-def test_MAE():
+@pytest.mark.parametrize(
+    "impl, sk_impl",
+    [
+        (MAE, mean_absolute_error),
+        (MSE, mean_squared_error),
+        (R2, r2_score),
+    ],
+)
+def test_regression(impl, sk_impl):
     y = np.array([1, 2, 3, 4, 5, 6, 7])
     y_hat = np.array([6, 5, 4, 2, 7, 8, 9])
-    rmse = MAE()
-    assert rmse(y, y_hat) == mean_absolute_error(y, y_hat)
+    impl = impl()
+    assert impl(y, y_hat) - sk_impl(y, y_hat) < 1e-7
 
 
-def test_R2():
-    y = np.array([1, 2, 3, 4, 5, 6, 7])
-    y_hat = np.array([6, 5, 4, 2, 7, 8, 9])
-    r2 = R2()
-    assert r2(y, y_hat) == r2_score(y, y_hat)
-
-
-def test_Accuracy():
+@pytest.mark.parametrize(
+    "impl, sk_impl",
+    [
+        (Accuracy, accuracy_score),
+        (Precision, partial(precision_score, average="micro")),
+        (Recall, partial(recall_score, average="micro")),
+        (F1, partial(f1_score, average="micro")),
+    ],
+)
+def test_classification(impl, sk_impl):
     y = np.array([2, 1, 1, 0])
     y_hat = np.array([0, 1, 2, 3])
-    acc = Accuracy()
-    assert acc(y, y_hat) == accuracy_score(y, y_hat)
-
-
-def test_Precision():
-    y = np.array([2, 1, 1, 0])
-    y_hat = np.array([0, 1, 2, 3])
-    prec = Precision()
-    assert prec(y, y_hat) == precision_score(y, y_hat, average='micro')
-
-
-def test_Recall():
-    y = np.array([2, 1, 1, 0])
-    y_hat = np.array([0, 1, 2, 3])
-    rec = Recall()
-    assert rec(y, y_hat) == recall_score(y, y_hat, average='micro')
-
-
-def test_F1():
-    y = np.array([2, 1, 1, 0])
-    y_hat = np.array([0, 1, 2, 3])
-    f1 = F1()
-    assert f1(y, y_hat) == f1_score(y, y_hat, average='micro')
-    
+    impl = impl()
+    assert impl(y, y_hat) - sk_impl(y, y_hat) < 1e-7
--- test/test_metrics_torch.py
+++ test/test_metrics_torch.py
@@ -1,16 +1,17 @@
+from functools import partial
+
 import torch
-from ml_metrics.metrics_torch import (F1, MAE, MSE, R2, RMSE, Accuracy,
-                                      Precision, Recall)
-from sklearn.metrics import (accuracy_score, f1_score, mean_absolute_error,
-                             mean_squared_error, precision_score, r2_score,
-                             recall_score)
-
-
-def test_MSE():
-    y = torch.FloatTensor([1, 2, 3, 4, 5, 6, 7])
-    y_hat = torch.FloatTensor([6, 5, 4, 2, 7, 8, 9])
-    mse = MSE()
-    assert mse(y, y_hat) == mean_squared_error(y, y_hat)
+import pytest
+from ml_metrics.metrics_torch import F1, MAE, MSE, R2, RMSE, Accuracy, Precision, Recall
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    mean_absolute_error,
+    mean_squared_error,
+    precision_score,
+    r2_score,
+    recall_score,
+)
 
 
 def test_RMSE():
@@ -20,43 +21,32 @@ def test_RMSE():
     assert rmse(y, y_hat) == mean_squared_error(y, y_hat) ** 0.5
 
 
-def test_MAE():
+@pytest.mark.parametrize(
+    "impl, sk_impl",
+    [
+        (MAE, mean_absolute_error),
+        (MSE, mean_squared_error),
+        (R2, r2_score),
+    ],
+)
+def test_regression(impl, sk_impl):
     y = torch.FloatTensor([1, 2, 3, 4, 5, 6, 7])
     y_hat = torch.FloatTensor([6, 5, 4, 2, 7, 8, 9])
-    rmse = MAE()
-    assert rmse(y, y_hat) == mean_absolute_error(y, y_hat)
+    impl = impl()
+    assert impl(y, y_hat) - sk_impl(y, y_hat) < 1e-7
 
 
-def test_R2():
-    y = torch.FloatTensor([1, 2, 3, 4, 5, 6, 7])
-    y_hat = torch.FloatTensor([6, 5, 4, 2, 7, 8, 9])
-    r2 = R2()
-    assert torch.abs(r2(y, y_hat) - r2_score(y, y_hat)) <= 1e-7
-
-
-def test_Accuracy():
+@pytest.mark.parametrize(
+    "impl, sk_impl",
+    [
+        (Accuracy, accuracy_score),
+        (Precision, partial(precision_score, average="micro")),
+        (Recall, partial(recall_score, average="micro")),
+        (F1, partial(f1_score, average="micro")),
+    ],
+)
+def test_classification(impl, sk_impl):
     y = torch.IntTensor([2, 1, 1, 0])
     y_hat = torch.IntTensor([0, 1, 2, 3])
-    acc = Accuracy()
-    assert acc(y, y_hat) == accuracy_score(y, y_hat)
-
-
-def test_Precision():
-    y = torch.IntTensor([2, 1, 1, 0])
-    y_hat = torch.IntTensor([0, 1, 2, 3])
-    prec = Precision()
-    assert prec(y, y_hat) == precision_score(y, y_hat, average='micro')
-
-
-def test_Recall():
-    y = torch.IntTensor([2, 1, 1, 0])
-    y_hat = torch.IntTensor([0, 1, 2, 3])
-    rec = Recall()
-    assert rec(y, y_hat) == recall_score(y, y_hat, average='micro')
-
-
-def test_F1():
-    y = torch.IntTensor([2, 1, 1, 0])
-    y_hat = torch.IntTensor([0, 1, 2, 3])
-    f1 = F1()
-    assert f1(y, y_hat) == f1_score(y, y_hat, average='micro')
+    impl = impl()
+    assert impl(y, y_hat) - sk_impl(y, y_hat) < 1e-7
